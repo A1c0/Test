78bdb55380a99c074ac251e98c3e1f2f51fb4197
alexConts <alexandrecontassot@hotmail.fr>
2018-02-19 11:46:58 +0100

    Ajout de somme de deux entiers

--- test.c
+++ test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int somme (int a , int b);
+
 int main(int argc, char const *argv[])
 {
 	printf("Hello Guys!\n");
+	int nombreA, nombreB;
+	printf("entrer un premier chiffre: ");
+	scanf("%d", &nombreA);
+	printf("entrer un deuxieme chiffre: ");
+	scanf("%d", &nombreB);
+	printf("le resultat de la somme des deux chiffres est :\n", somme(nombreA, nombreB));
 	return 0;
+}
+
+int somme (int a , int b)
+{
+	return a+b;
 }
